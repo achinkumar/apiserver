51c792a70d498961bb090a5970120f334f116678
achin <achin@achin-Lenovo-U410.(none)>
2013-12-19 21:47:41 +0530

    improved

--- app.js
+++ app.js
@@ -13,26 +13,36 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'public'));
+// app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(__dirname + '/public'));
+//app.use(express.static(path.join(__dirname, '/public')));
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function (req, res)
+{
+    res.render('api.html');
+});
+
+
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-
-app.get('/', routes.index);
 app.get('/users', user.list);
-app.get('/paycart',routes.paycart);
+app.get('/', routes.index);
 app.get('/employee',routes.viewEmployee);
-app.put('/employee',routes.updatEmployee);
+app.put('/employee',routes.updateEmployee);
+app.post('/employee',routes.createEmployee);
+app.delete('/employee',routes.deleteEmployee);
+app.get('/employeeErr',routes.errorEmployee);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,27 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
-exports.paycart = function(req, res){
-  res.render('paycart', { title: 'Paycart' });
-};
+
+
 exports.viewEmployee =function(req,res){
 	var employee = {"mode":"view","data":{"emp_id":"1","name":"achin"}};
 	res.json(employee);
-}
+};
+
+exports.updateEmployee =function(req,res){
+	var employee = {"mode":"update","msg":"employee records updated","data":{"emp_id":"1","name":"achin"}};
+	res.json(employee);
+};
+exports.createEmployee =function(req,res){
+	var employee = {"mode":"create","msg":"New employee created","data":{"emp_id":"1","name":"achin"}};
+	res.json(employee);
+};
+exports.deleteEmployee =function(req,res){
+	var employee = {"error":"no employee with this id","mode":"delete","msg":"employee record deleted","data":{"emp_id":"1","name":"achin"}};
+	res.json(employee);
+};
+exports.errorEmployee =function(req,res){
+	var employeeErr = {"error":"no employee with this id"};
+	res.json(employeeErr);
+};
+
